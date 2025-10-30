928bbe41ab8e10f1bebd516eaa7c782c360df67a
xutu <cnxutu@gmail.com>
2025-10-30 10:59:25 +0800

    feat(form-dict): 进行整体字段优化1.1

--- solution-auth/form-dict-sample/src/main/java/com/cv/solution/formdict/form/common/enums/InputSubTypeEnum.java
+++ solution-auth/form-dict-sample/src/main/java/com/cv/solution/formdict/form/common/enums/InputSubTypeEnum.java
@@ -6,7 +6,7 @@ package com.cv.solution.formdict.form.common.enums;
  */
 public enum InputSubTypeEnum {
 
-    VALIDATE_NULL(0, "输入框-子类校验器-不作处理"),
+    VALIDATE_NOTHING(0, "输入框-子类校验器-不作处理"),
     VALIDATE_PHONE(1, "输入框-子类校验器-手机号校验"),
     VALIDATE_ID_CARD(2, "输入框-子类校验器-身份证号校验"),
     VALIDATE_EMAIL(3, "输入框-子类校验器-邮箱校验"),
